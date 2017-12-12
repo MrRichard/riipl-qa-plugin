ed19339b598c10df070d588b59c60bcb4315af82
Richard Barcus <richard.barcus@gmail.com>
2017-12-12 14:32:11 -0500

    Converting XNAT template to new riipl-qa-plugin

---
+++ src/main/java/org/nrg/xnat/plugins/RiiplQAPlugin/plugin/RiiplQAPlugin.java
@@ -0,0 +1,23 @@
+/*
+ * xnat-template: org.nrg.xnat.plugins.template.plugin.XnatTemplatePlugin
+ * XNAT http://www.xnat.org
+ * Copyright (c) 2017, Washington University School of Medicine
+ * All Rights Reserved
+ *
+ * Released under the Simplified BSD.
+ */
+
+package org.nrg.xnat.plugins.RiiplQAPlugin.plugin;
+import org.nrg.framework.annotations.XnatPlugin;
+
+/**
+ * @author rbarcus
+ */
+@XnatPlugin(
+	value = "riipl-qa-plugin",
+	name = "RIIPL XNAT QA Plugin Ver 0.1",
+	description = "Plugin for RIIPL Pipeline-specific QA"
+)
+
+public class RiiplQAPlugin{	
+}
