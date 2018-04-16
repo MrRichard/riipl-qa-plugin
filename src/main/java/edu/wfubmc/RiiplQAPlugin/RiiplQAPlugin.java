17108716a2c876a0ef054ed009e91e3e067947db
Richard Barcus <rbarcus@wakehealth.edu>
2018-04-16 13:02:06 -0400

    added PCASL PVC WM and GM. commiting to repo to test build against windows builder

--- src/main/java/edu/wfubmc/RiiplQAPlugin/RiiplQAPlugin.java
+++ src/main/java/edu/wfubmc/RiiplQAPlugin/RiiplQAPlugin.java
@@ -15,7 +15,7 @@ import org.nrg.framework.annotations.XnatDataModel;
  */
 @XnatPlugin(
 	value = "riipl-qa-plugin",
-	name = "RIIPL XNAT QA Plugin Ver 0.0.2",
+	name = "RIIPL XNAT QA Plugin Ver 0.0.3",
 	description = "Plugin for RIIPL Pipeline-specific QA",
 	dataModels = {
 			@XnatDataModel(
@@ -48,7 +48,6 @@ import org.nrg.framework.annotations.XnatDataModel;
 					plural = "QC NODDI CAT12 Seg Reports",
 					code = "QA"
 				),
-			
 	}
 )
 
