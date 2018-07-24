8c13e5a7bb1987b099a60c0312a3053789bfc1e7
Richard Alan Barcus <rbarcus@wakehealth.edu>
2018-07-24 08:58:42 -0400

    Added a fix for the updating the report date. May have to fix "Insert Date"

---
+++ src/main/java/org/nrg/xdat/turbine/modules/screens/XDATScreen_edit_riiplqa_SegCat12Data.java
@@ -0,0 +1,43 @@
+//package org.nrg.xdat.turbine.modules.screens;
+/*
+ * GENERATED FILE
+ * Created on Mon Jul 23 13:17:01 EDT 2018
+ *
+ */
+//package org.nrg.xdat.turbine.modules.screens;
+import org.apache.turbine.util.RunData;
+import org.apache.velocity.context.Context;
+import org.nrg.xdat.turbine.utils.TurbineUtils;
+import org.nrg.xft.ItemI;
+import org.nrg.xft.XFTItem;
+import org.apache.velocity.tools.generic.DateTool;
+
+
+/**
+ * @author XDAT
+ *
+ */
+public class XDATScreen_edit_riiplqa_SegCat12Data extends org.nrg.xnat.turbine.modules.screens.EditImageAssessorScreen {
+	static org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(XDATScreen_edit_riiplqa_SegCat12Data.class);
+	/* (non-Javadoc)
+	 * @see org.nrg.xdat.turbine.modules.screens.EditScreenA#getElementName()
+	 */
+	public String getElementName() {
+	    return "riiplqa:SegCat12Data";
+	}
+	
+	public ItemI getEmptyItem(RunData data) throws Exception
+	{
+		return super.getEmptyItem(data);
+	}
+	/* (non-Javadoc)
+	 * @see org.nrg.xdat.turbine.modules.screens.SecureReport#finalProcessing(org.apache.turbine.util.RunData, org.apache.velocity.context.Context)
+	 */
+	public void finalProcessing(RunData data, Context context) {
+		try{
+	 		context.put("date" ,  new DateTool());
+	 		System.out.println("Loaded date object");
+		} catch(Exception e){
+			System.out.println("did not load date object");
+		}
+	}}
